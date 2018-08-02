1f533c1dd32191d85aa9b311b4c1ce754244da64
yousuf451 <yousufshaikh92@gmail.com>
2018-08-02 21:33:43 +0500

    fetch add

--- sw.js
+++ sw.js
@@ -15,3 +15,16 @@ self.addEventListener('install', function(e) {
     })
   );
 });
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
